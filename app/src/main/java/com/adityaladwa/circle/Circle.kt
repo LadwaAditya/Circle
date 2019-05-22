13f0366869adadb0a5ff738247795abd84cc2f6a
Aditya Ladwa <ladwa.aditya@gmail.com>
2019-05-22 21:45:36 +0530

    Feat: Circle, Random color on clicking circle
    A random number color is generated and assigned to circle when clicks
    are detected within the circle and not outside it

--- app/src/main/java/com/adityaladwa/circle/Circle.kt
+++ app/src/main/java/com/adityaladwa/circle/Circle.kt
@@ -5,7 +5,11 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
+import kotlin.random.Random
+
+private val TAG = Circle::class.java.simpleName
 
 class Circle @JvmOverloads constructor(
     context: Context?,
@@ -15,15 +19,42 @@ class Circle @JvmOverloads constructor(
 ) : View(context, attrs, defStyleAttr) {
 
     private val paint = Paint()
+    private var cX: Float = 0f
+    private var cY: Float = 0f
 
     init {
-        paint.color = Color.RED
+        paint.color = Color.BLUE
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-        canvas?.let {
-            canvas.drawCircle(width / 2f, height / 2f, width / 2f, paint)
+        canvas?.run {
+            cX = width / 2f
+            cY = height / 2f
+            canvas.drawCircle(cX, cY, cX, paint)
         }
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        event?.run {
+            if (isPointInCircle(x, y)) {
+                paint.color = getNewColor()
+                invalidate()
+            }
+        }
+        return super.onTouchEvent(event)
+    }
+
+    private fun getNewColor(): Int {
+        return Color.rgb(Random.nextInt(0, 250), Random.nextInt(0, 250), Random.nextInt(0, 250))
+    }
+
+    private fun isPointInCircle(x: Float, y: Float): Boolean {
+        val dx = x - cX
+        val dy = y - cY
+        if ((dx * dx) + (dy * dy) <= (cX * cX)) {
+            return true
+        }
+        return false
     }
 }
